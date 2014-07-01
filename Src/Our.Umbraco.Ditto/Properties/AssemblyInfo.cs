27e3fad8ef61add130d757c00cc7d06a396c756a
leekelleher <leekelleher@gmail.com>
2014-07-01 06:52:38 +0100

    Bumped version number - v0.3.0

--- Src/Our.Umbraco.Ditto/Properties/AssemblyInfo.cs
+++ Src/Our.Umbraco.Ditto/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("C3A53472-6FBC-42ED-ABD4-5B1594C32E98")]
 
-[assembly: AssemblyVersion("0.2.1")]
-[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
