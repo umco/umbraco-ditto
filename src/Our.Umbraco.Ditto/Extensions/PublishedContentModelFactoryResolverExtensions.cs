e363448ca201fe4f773f6f54f7d3490202f59279
leekelleher <leekelleher@gmail.com>
2014-07-23 11:36:48 +0100

    Added extension method for `PublishedContentModelFactoryResolver`.
    To register the Ditto model-factory.

---
+++ src/Our.Umbraco.Ditto/Extensions/PublishedContentModelFactoryResolverExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Our.Umbraco.Ditto
+{
+	public static class PublishedContentModelFactoryResolverExtensions
+	{
+		public static void SetFactory<T>(this PublishedContentModelFactoryResolver resolver)
+			where T : IPublishedContent
+		{
+			var types = PluginManager.Current.ResolveTypes<T>();
+			var factory = new DittoPublishedContentModelFactory(types);
+
+			resolver.SetFactory(factory);
+		}
+	}
+}
