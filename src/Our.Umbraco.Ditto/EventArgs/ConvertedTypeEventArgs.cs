fa44d09178b580594da9f3722dd2d4177d7585d2
James South <james_south@hotmail.com>
2014-11-12 23:13:31 +0000

    Fixes project to conform to StyleCop rules
    This commit does several things.
    1. Adds xml documentation
    2. Fixes braces, usings and naming conventions
    3. Places classes in the correct respective namespaces.
    
    I am aware that this breaks the layout of public methods. This is better
    for allowing further extension of the project and since it is still an
    early pre-release I think it is acceptable at this juncture.

--- src/Our.Umbraco.Ditto/EventArgs/ConvertedTypeEventArgs.cs
+++ src/Our.Umbraco.Ditto/EventArgs/ConvertedTypeEventArgs.cs
@@ -1,14 +1,36 @@
-﻿using System;
-using Umbraco.Core.Models;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConvertedTypeEventArgs.cs" company="Umbrella Inc, Our Umbraco and other contributors">
+//   Copyright Umbrella Inc, Our Umbraco and other contributors
+// </copyright>
+// <summary>
+//   Provides data for a converted event.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
-namespace Our.Umbraco.Ditto
+namespace Our.Umbraco.Ditto.EventArgs
 {
+    using System;
+
+    using global::Umbraco.Core.Models;
+
+    /// <summary>
+    /// Provides data for a converted event.
+    /// </summary>
     public class ConvertedTypeEventArgs : EventArgs
     {
+        /// <summary>
+        /// Gets or sets the content.
+        /// </summary>
         public IPublishedContent Content { get; set; }
 
+        /// <summary>
+        /// Gets or sets the converted object.
+        /// </summary>
         public object Converted { get; set; }
 
+        /// <summary>
+        /// Gets or sets the converted type.
+        /// </summary>
         public Type ConvertedType { get; set; }
     }
 }
--- src/Our.Umbraco.Ditto/Extensions/PublishedContentExtensions.cs
+++ src/Our.Umbraco.Ditto/Extensions/PublishedContentExtensions.cs
@@ -1,33 +1,142 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using Umbraco.Core;
-using Umbraco.Core.Models;
-using Umbraco.Web;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PublishedContentExtensions.cs" company="Umbrella Inc, Our Umbraco and other contributors">
+//   Copyright Umbrella Inc, Our Umbraco and other contributors
+// </copyright>
+// <summary>
+//   Encapsulates extension methods for <see cref="IPublishedContent" />.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
-namespace Our.Umbraco.Ditto
+namespace Our.Umbraco.Ditto.Extensions
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Reflection;
+
+    using Our.Umbraco.Ditto.Attributes;
+    using Our.Umbraco.Ditto.EventArgs;
+
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Models;
+    using global::Umbraco.Web;
+
+    /// <summary>
+    /// Encapsulates extension methods for <see cref="IPublishedContent"/>.
+    /// </summary>
     public static class PublishedContentExtensions
     {
-        internal static object As(this IPublishedContent content, Type type,
+        /// <summary>
+        /// Returns the given instance of <see cref="IPublishedContent"/> as the specified type.
+        /// </summary>
+        /// <param name="content">
+        /// The <see cref="IPublishedContent"/> to convert.
+        /// </param>
+        /// <param name="convertingType">
+        /// The <see cref="Action{ConvertingTypeEventArgs}"/> to fire when converting.
+        /// </param>
+        /// <param name="convertedType">
+        /// The <see cref="Action{ConvertedTypeEventArgs}"/> to fire when converted.
+        /// </param>
+        /// <typeparam name="T">
+        /// The <see cref="Type"/> of items to return.
+        /// </typeparam>
+        /// <returns>
+        /// The resolved <see cref="T"/>.
+        /// </returns>
+        public static T As<T>(
+            this IPublishedContent content,
+            Action<ConvertingTypeEventArgs> convertingType = null,
+            Action<ConvertedTypeEventArgs> convertedType = null)
+            where T : class
+        {
+            return content.As(typeof(T), convertingType, convertedType) as T;
+        }
+
+        /// <summary>
+        /// Gets a collection of the given type from the given <see cref="IEnumerable{IPublishedContent}"/>.
+        /// </summary>
+        /// <param name="items">
+        /// The <see cref="IEnumerable{IPublishedContent}"/> to convert.
+        /// </param>
+        /// <param name="documentTypeAlias">
+        /// The document type alias.
+        /// </param>
+        /// <param name="convertingType">
+        /// The <see cref="Action{ConvertingTypeEventArgs}"/> to fire when converting.
+        /// </param>
+        /// <param name="convertedType">
+        /// The <see cref="Action{ConvertedTypeEventArgs}"/> to fire when converted.
+        /// </param>
+        /// <typeparam name="T">
+        /// The <see cref="Type"/> of items to return.
+        /// </typeparam>
+        /// <returns>
+        /// The resolved <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public static IEnumerable<T> As<T>(
+            this IEnumerable<IPublishedContent> items,
+            string documentTypeAlias = null,
+            Action<ConvertingTypeEventArgs> convertingType = null,
+            Action<ConvertedTypeEventArgs> convertedType = null)
+            where T : class
+        {
+            using (DisposableTimer.DebugDuration<IEnumerable<T>>(string.Format("IEnumerable As ({0})", documentTypeAlias)))
+            {
+                if (string.IsNullOrWhiteSpace(documentTypeAlias))
+                {
+                    return items.Select(x => x.As<T>(convertingType, convertedType));
+                }
+
+                return items
+                    .Where(x => documentTypeAlias.InvariantEquals(x.DocumentTypeAlias))
+                    .Select(x => x.As<T>(convertingType, convertedType));
+            }
+        }
+
+        /// <summary>
+        /// Returns an object representing the given <see cref="Type"/>.
+        /// </summary>
+        /// <param name="content">
+        /// The <see cref="IPublishedContent"/> to convert.
+        /// </param>
+        /// <param name="type">
+        /// The <see cref="Type"/> of items to return.
+        /// </param>
+        /// <param name="convertingType">
+        /// The <see cref="Action{ConvertingTypeEventArgs}"/> to fire when converting.
+        /// </param>
+        /// <param name="convertedType">
+        /// The <see cref="Action{ConvertedTypeEventArgs}"/> to fire when converted.
+        /// </param>
+        /// <returns>
+        /// The converted <see cref="Object"/> as the given type.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the given type has invalid constructors.
+        /// </exception>
+        internal static object As(
+            this IPublishedContent content,
+            Type type,
             Action<ConvertingTypeEventArgs> convertingType = null,
             Action<ConvertedTypeEventArgs> convertedType = null)
         {
             object instance = null;
 
             if (content == null)
+            {
                 return instance;
+            }
 
             using (DisposableTimer.DebugDuration(type, string.Format("IPublishedContent As ({0})", content.DocumentTypeAlias), "Complete"))
             {
-                var constructor = type.GetConstructors()
+                ConstructorInfo constructor = type.GetConstructors()
                     .OrderBy(x => x.GetParameters().Length)
                     .First();
-                var constructorParams = constructor.GetParameters();
+                ParameterInfo[] constructorParams = constructor.GetParameters();
 
-                var args1 = new ConvertingTypeEventArgs { Content = content };
+                ConvertingTypeEventArgs args1 = new ConvertingTypeEventArgs { Content = content };
 
                 EventHandlers.CallConvertingTypeHandler(args1);
 
@@ -50,24 +159,26 @@ namespace Our.Umbraco.Ditto
                     throw new InvalidOperationException("Type {0} has invalid constructor parameters");
                 }
 
-                var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-                var contentType = content.GetType();
+                PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                Type contentType = content.GetType();
 
-                foreach (var propertyInfo in properties.Where(x => x.CanWrite))
+                foreach (PropertyInfo propertyInfo in properties.Where(x => x.CanWrite))
                 {
                     using (DisposableTimer.DebugDuration(type, string.Format("ForEach Property ({1} {0})", propertyInfo.Name, content.Id), "Complete"))
                     {
-                        // check for the ignore attribute
-                        var ignoreAttr = propertyInfo.GetCustomAttribute<DittoIgnoreAttribute>();
+                        // Check for the ignore attribute.
+                        DittoIgnoreAttribute ignoreAttr = propertyInfo.GetCustomAttribute<DittoIgnoreAttribute>();
                         if (ignoreAttr != null)
+                        {
                             continue;
+                        }
 
-                        var umbracoPropertyName = propertyInfo.Name;
-                        var altUmbracoPropertyName = string.Empty;
-                        var recursive = false;
+                        string umbracoPropertyName = propertyInfo.Name;
+                        string altUmbracoPropertyName = string.Empty;
+                        bool recursive = false;
                         object defaultValue = null;
 
-                        var umbracoPropertyAttr = propertyInfo.GetCustomAttribute<UmbracoPropertyAttribute>();
+                        UmbracoPropertyAttribute umbracoPropertyAttr = propertyInfo.GetCustomAttribute<UmbracoPropertyAttribute>();
                         if (umbracoPropertyAttr != null)
                         {
                             umbracoPropertyName = umbracoPropertyAttr.PropertyName;
@@ -76,28 +187,28 @@ namespace Our.Umbraco.Ditto
                             defaultValue = umbracoPropertyAttr.DefaultValue;
                         }
 
-                        // Try fetching the value
-                        var contentProperty = contentType.GetProperty(umbracoPropertyName);
-                        var propertyValue = contentProperty != null
-                            ? contentProperty.GetValue(content, null)
-                            : content.GetPropertyValue(umbracoPropertyName,
-                                recursive);
+                        // Try fetching the value.
+                        PropertyInfo contentProperty = contentType.GetProperty(umbracoPropertyName);
+                        object propertyValue = contentProperty != null
+                                                ? contentProperty.GetValue(content, null)
+                                                : content.GetPropertyValue(umbracoPropertyName, recursive);
 
-                        // Try fetching the alt value
+                        // Try fetching the alt value.
                         if (propertyValue == null && !string.IsNullOrWhiteSpace(altUmbracoPropertyName))
                         {
                             contentProperty = contentType.GetProperty(altUmbracoPropertyName);
                             propertyValue = contentProperty != null
-                                ? contentProperty.GetValue(content, null)
-                                : content.GetPropertyValue(altUmbracoPropertyName,
-                                    recursive);
+                                                ? contentProperty.GetValue(content, null)
+                                                : content.GetPropertyValue(altUmbracoPropertyName, recursive);
                         }
 
-                        // Try setting the default value
+                        // Try setting the default value.
                         if (propertyValue == null && defaultValue != null)
+                        {
                             propertyValue = defaultValue;
+                        }
 
-                        // Process the value
+                        // Process the value.
                         if (propertyValue != null)
                         {
                             if (propertyInfo.PropertyType.IsInstanceOfType(propertyValue))
@@ -108,15 +219,15 @@ namespace Our.Umbraco.Ditto
                             {
                                 using (DisposableTimer.DebugDuration(type, string.Format("TypeConverter ({0}, {1})", content.Id, propertyInfo.Name), "Complete"))
                                 {
-                                    var converterAttr = propertyInfo.GetCustomAttribute<TypeConverterAttribute>();
+                                    TypeConverterAttribute converterAttr = propertyInfo.GetCustomAttribute<TypeConverterAttribute>();
                                     if (converterAttr != null)
                                     {
-                                        var converter = Activator.CreateInstance(Type.GetType(converterAttr.ConverterTypeName)) as TypeConverter;
+                                        TypeConverter converter = Activator.CreateInstance(Type.GetType(converterAttr.ConverterTypeName)) as TypeConverter;
                                         propertyInfo.SetValue(instance, converter.ConvertFrom(propertyValue), null);
                                     }
                                     else
                                     {
-                                        var convert = propertyValue.TryConvertTo(propertyInfo.PropertyType);
+                                        Attempt<object> convert = propertyValue.TryConvertTo(propertyInfo.PropertyType);
                                         if (convert.Success)
                                         {
                                             propertyInfo.SetValue(instance, convert.Result, null);
@@ -128,7 +239,7 @@ namespace Our.Umbraco.Ditto
                     }
                 }
 
-                var args2 = new ConvertedTypeEventArgs
+                ConvertedTypeEventArgs args2 = new ConvertedTypeEventArgs
                 {
                     Content = content,
                     Converted = instance,
@@ -136,36 +247,13 @@ namespace Our.Umbraco.Ditto
                 };
 
                 if (convertedType != null)
+                {
                     convertedType(args2);
+                }
 
                 EventHandlers.CallConvertedTypeHandler(args2);
 
                 return args2.Converted;
-            }
-        }
-
-        public static T As<T>(this IPublishedContent content,
-            Action<ConvertingTypeEventArgs> convertingType = null,
-            Action<ConvertedTypeEventArgs> convertedType = null)
-            where T : class
-        {
-            return content.As(typeof(T), convertingType, convertedType) as T;
-        }
-
-        public static IEnumerable<T> As<T>(this IEnumerable<IPublishedContent> items,
-            string documentTypeAlias = null,
-            Action<ConvertingTypeEventArgs> convertingType = null,
-            Action<ConvertedTypeEventArgs> convertedType = null)
-            where T : class
-        {
-            using (DisposableTimer.DebugDuration<IEnumerable<T>>(string.Format("IEnumerable As ({0})", documentTypeAlias)))
-            {
-                if (string.IsNullOrWhiteSpace(documentTypeAlias))
-                    return items.Select(x => x.As<T>(convertingType, convertedType));
-
-                return items
-                    .Where(x => documentTypeAlias.InvariantEquals(x.DocumentTypeAlias))
-                    .Select(x => x.As<T>(convertingType, convertedType));
             }
         }
     }
