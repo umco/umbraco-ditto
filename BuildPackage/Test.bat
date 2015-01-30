9c5883dee594dab2ddc417af4ed4c52668c93fa7
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-01-30 19:04:20 +0000

    Move Umbraco Min Version into package.proj

--- BuildPackage/Test.bat
+++ BuildPackage/Test.bat
@@ -1,6 +1,5 @@
 set APPVEYOR_BUILD_NUMBER=100
 set APPVEYOR_BUILD_VERSION=0.5.1.100
-set UMBRACO_PACKAGE_MIN_VERSION=7.1.4
 set UMBRACO_PACKAGE_PRERELEASE_SUFFIX=beta
 REM ** Generally branch should be either master or Release **
 set APPVEYOR_REPO_BRANCH=release
