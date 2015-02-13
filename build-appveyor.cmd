c14e66d1078046cc9af67c4f0232e6c9645e59a8
leekelleher <leekelleher@gmail.com>
2015-02-13 12:49:28 +0000

    Modified build batch files, as I'm still running VS2012,
    and the MSBuild path (for VS2013) wasn't available on my machine.
    Using the Window's .NET 4.0 path to locate the MSBuild exec.

--- build-appveyor.cmd
+++ build-appveyor.cmd
@@ -3,5 +3,5 @@ ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
 ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 
-Call src\.nuget\NuGet.exe restore src\Our.Umbraco.Ditto.sln
-Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" build\package.proj
+CALL src\.nuget\NuGet.exe restore src\Our.Umbraco.Ditto.sln
+CALL "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" build\package.proj
--- build-local.cmd
+++ build-local.cmd
@@ -1,20 +1,24 @@
-echo off
+ECHO off
 
-set /P APPVEYOR_BUILD_NUMBER=Please enter a build number (e.g. 134):
-set /P PACKAGE_VERISON=Please enter your package version (e.g. 1.0.5):
-set /P UMBRACO_PACKAGE_PRERELEASE_SUFFIX=Please enter your package release suffix or leave empty (e.g. beta):
+SET /P APPVEYOR_BUILD_NUMBER=Please enter a build number (e.g. 134):
+SET /P PACKAGE_VERISON=Please enter your package version (e.g. 1.0.5):
+SET /P UMBRACO_PACKAGE_PRERELEASE_SUFFIX=Please enter your package release suffix or leave empty (e.g. beta):
 
-set /P APPVEYOR_REPO_TAG=If you want to simulate a GitHub tag for a release (e.g. true):
+SET /P APPVEYOR_REPO_TAG=If you want to simulate a GitHub tag for a release (e.g. true):
 
 if "%APPVEYOR_BUILD_NUMBER%" == "" (
-  set APPVEYOR_BUILD_NUMBER=100
+  SET APPVEYOR_BUILD_NUMBER=100
 )
 if "%PACKAGE_VERISON%" == "" (
-  set PACKAGE_VERISON=0.1.0
+  SET PACKAGE_VERISON=0.1.0
 )
 
-set APPVEYOR_BUILD_VERSION=%PACKAGE_VERISON%.%APPVEYOR_BUILD_NUMBER%
+SET APPVEYOR_BUILD_VERSION=%PACKAGE_VERISON%.%APPVEYOR_BUILD_NUMBER%
 
 build-appveyor.cmd
 
-exit
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@EXIT /B 0
+:err
+@PAUSE
+@EXIT /B 1
