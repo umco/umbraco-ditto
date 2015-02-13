1f060701817b764f88b35ca6577b7cd825ad4f75
leekelleher <leekelleher@gmail.com>
2015-02-13 12:46:32 +0000

    Modified the MSBuild script to handle the AssemblyVersion number differently.
    (Uses the [major.minor.*] wildcard approach)

--- src/Our.Umbraco.Ditto/Properties/VersionInfo.cs
+++ src/Our.Umbraco.Ditto/Properties/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.5.*")]
-[assembly: AssemblyInformationalVersion("0.5.0")]
+[assembly: AssemblyVersion("0.6.*")]
+[assembly: AssemblyInformationalVersion("0.6.0-develop0")]
 
 
