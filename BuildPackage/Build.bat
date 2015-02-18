6c09a1d1d39c530d3de40a1ee347f36be7c2fe42
leekelleher <leekelleher@gmail.com>
2015-02-18 14:14:16 +0000

    Amends to the MSBuild scripts, to support VS2012.

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -4,4 +4,4 @@ ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 cd ..\BuildPackage\
 Call Tools\nuget.exe restore ..\Src\Our.Umbraco.Ditto.sln
-Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" package.proj
+Call "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" package.proj
