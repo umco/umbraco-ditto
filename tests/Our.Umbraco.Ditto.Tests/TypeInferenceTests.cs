0f334a073cab5bcd4d55fabdc8e9a6f8a75f054a
James South <james_south@hotmail.com>
2015-03-04 00:55:24 +0000

    Adding tests
    Very limited so far, I always get stuck dreaming of test cases.

---
+++ tests/Our.Umbraco.Ditto.Tests/TypeInferenceTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Our.Umbraco.Ditto.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The type inference tests.
+    /// </summary>
+    [TestFixture]
+    public class TypeInferenceTests
+    {
+        /// <summary>
+        /// Tests the <see cref="TypeInferenceExtensions.IsCollectionType"/> method.
+        /// </summary>
+        /// <param name="input">
+        /// The input <see cref="Type"/>.
+        /// </param>
+        /// <param name="expected">
+        /// The expected result.
+        /// </param>
+        [Test]
+        [TestCase(typeof(string), false)]
+        [TestCase(typeof(bool), false)]
+        [TestCase(typeof(int), false)]
+        [TestCase(typeof(long), false)]
+        [TestCase(typeof(List<string>), true)]
+        [TestCase(typeof(Collection<string>), true)]
+        [TestCase(typeof(HashSet<string>), true)]
+        [TestCase(typeof(Enumerable), false)]
+        [TestCase(typeof(Dictionary<string, string>), true)]
+        public void TestIsCollectionType(Type input, bool expected)
+        {
+            Assert.AreEqual(input.IsCollectionType(), expected);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="TypeInferenceExtensions.IsEnumerableType"/> method.
+        /// </summary>
+        /// <param name="input">
+        /// The input <see cref="Type"/>.
+        /// </param>
+        /// <param name="expected">
+        /// The expected result.
+        /// </param>
+        [Test]
+        [TestCase(typeof(string), true)]
+        [TestCase(typeof(bool), false)]
+        [TestCase(typeof(int), false)]
+        [TestCase(typeof(long), false)]
+        [TestCase(typeof(List<string>), true)]
+        [TestCase(typeof(Collection<string>), true)]
+        [TestCase(typeof(HashSet<string>), true)]
+        [TestCase(typeof(Enumerable), false)]
+        [TestCase(typeof(Dictionary<string, string>), true)]
+        public void TestIsEnumerableType(Type input, bool expected)
+        {
+            Assert.AreEqual(input.IsEnumerableType(), expected);
+        }
+    }
+}
