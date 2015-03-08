f0ccd6e7b144b53b73630637561aa3870cd18bd7
leekelleher <leekelleher@gmail.com>
2015-03-08 22:03:40 +0000

    [WIP] Started dev on DictionaryValue TypeConverter.

---
+++ src/Our.Umbraco.Ditto/ComponentModel/TypeConverters/DictionaryValueConverter.cs
@@ -0,0 +1,30 @@
+﻿namespace Our.Umbraco.Ditto
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.Linq;
+
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Models;
+
+    public class DictionaryValueConverter : TypeConverterWithAttribute<UmbracoDictionaryValueAttribute>
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (context != null && context.PropertyDescriptor != null && context.PropertyDescriptor.Attributes != null)
+            {
+                var attributes = context.PropertyDescriptor.Attributes.OfType<UmbracoDictionaryValueAttribute>();
+
+                if (attributes.Any())
+                {
+                    var dictionaryValueAttribute = attributes.Cast<UmbracoDictionaryValueAttribute>().First();
+
+                    return ConverterHelper.UmbracoHelper.GetDictionaryValue(dictionaryValueAttribute.DictionaryKey);
+                }
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
---
+++ src/Our.Umbraco.Ditto/ComponentModel/TypeConverters/TypeConverterWithAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace Our.Umbraco.Ditto
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+
+    public class TypeConverterWithAttribute<T> : TypeConverter where T : Attribute
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (context != null && context.PropertyDescriptor != null && context.PropertyDescriptor.Attributes != null)
+            {
+                return context.PropertyDescriptor.Attributes
+                    .OfType<T>()
+                    .Any();
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+    }
+}
