b557c6e0561d87ca7b86cc1cbae7703c593637af
leekelleher <leekelleher@gmail.com>
2015-04-14 15:51:14 +0100

    Added XML comments

--- src/Our.Umbraco.Ditto/ComponentModel/TypeConverters/DittoConverterWithAttribute.cs
+++ src/Our.Umbraco.Ditto/ComponentModel/TypeConverters/DittoConverterWithAttribute.cs
@@ -4,8 +4,20 @@
     using System.ComponentModel;
     using System.Linq;
 
+    /// <summary>
+    /// An extension of the <see cref="DittoConverter"/> class which accepts an <see cref="System.Attribute"/> as a generic type.
+    /// </summary>
+    /// <typeparam name="T">An <see cref="System.Attribute"/> type</typeparam>
     public abstract class DittoConverterWithAttribute<T> : DittoConverter where T : Attribute
     {
+        /// <summary>
+        /// Returns whether this converter can convert an object of the given type to the type of this converter, using the specified context.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.ComponentModel.ITypeDescriptorContext" /> that provides a format context.</param>
+        /// <param name="sourceType">A <see cref="T:System.Type" /> that represents the type you want to convert from.</param>
+        /// <returns>
+        /// true if this converter can perform the conversion; otherwise, false.
+        /// </returns>
         public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
         {
             if (context != null && context.PropertyDescriptor != null && context.PropertyDescriptor.Attributes != null)
@@ -18,6 +30,11 @@
             return base.CanConvertFrom(context, sourceType);
         }
 
+        /// <summary>
+        /// Gets the attribute.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
         public T GetAttribute(ITypeDescriptorContext context)
         {
             if (context != null && context.PropertyDescriptor != null && context.PropertyDescriptor.Attributes != null)
--- src/Our.Umbraco.Ditto/ComponentModel/TypeConverters/DittoDictionaryValueConverter.cs
+++ src/Our.Umbraco.Ditto/ComponentModel/TypeConverters/DittoDictionaryValueConverter.cs
@@ -6,8 +6,19 @@
 
     using global::Umbraco.Web;
 
+    /// <summary>
+    /// Provides a unified way of retrieving an Umbraco dictionary item value for a model property.
+    /// </summary>
     public class DittoDictionaryValueConverter : DittoConverterWithAttribute<UmbracoDictionaryValueAttribute>
     {
+        /// <summary>
+        /// Returns whether this converter can convert the object to the specified type, using the specified context.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.ComponentModel.ITypeDescriptorContext" /> that provides a format context.</param>
+        /// <param name="destinationType">A <see cref="T:System.Type" /> that represents the type you want to convert to.</param>
+        /// <returns>
+        /// true if this converter can perform the conversion; otherwise, false.
+        /// </returns>
         public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
         {
             if (destinationType == typeof(string))
@@ -18,6 +29,15 @@
             return base.CanConvertTo(context, destinationType);
         }
 
+        /// <summary>
+        /// Converts the given object to the type of this converter, using the specified context and culture information.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.ComponentModel.ITypeDescriptorContext" /> that provides a format context.</param>
+        /// <param name="culture">The <see cref="T:System.Globalization.CultureInfo" /> to use as the current culture.</param>
+        /// <param name="value">The <see cref="T:System.Object" /> to convert.</param>
+        /// <returns>
+        /// An <see cref="T:System.Object" /> that represents the converted value.
+        /// </returns>
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
             var dictionaryValueAttribute = this.GetAttribute(context);
