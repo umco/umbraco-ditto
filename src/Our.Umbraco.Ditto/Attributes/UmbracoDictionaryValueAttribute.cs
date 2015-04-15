6a6f66efe5d1241a04037fdb7af0cdcf5d3c17d0
leekelleher <leekelleher@gmail.com>
2015-04-15 11:21:09 +0100

    [WIP] [UNTESTED] Trying out the `UmbracoDictionaryValueAttribute`
    [skip ci]

--- src/Our.Umbraco.Ditto/Attributes/UmbracoDictionaryValueAttribute.cs
+++ src/Our.Umbraco.Ditto/Attributes/UmbracoDictionaryValueAttribute.cs
@@ -1,13 +1,18 @@
 ﻿namespace Our.Umbraco.Ditto
 {
     using System;
+    using System.Globalization;
+    using System.Reflection;
+
+    using global::Umbraco.Core.Models;
+    using global::Umbraco.Web;
 
     /// <summary>
     /// The Umbraco dictionary value attribute.
     /// Used for providing Umbraco with additional information about a dictionary item to aid property value conversion.
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class UmbracoDictionaryValueAttribute : Attribute
+    public class UmbracoDictionaryValueAttribute : DittoValueAttribute
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="UmbracoDictionaryValueAttributeAttribute"/> class.
@@ -24,5 +29,18 @@
         /// Gets or sets the dictionary key.
         /// </summary>
         public string DictionaryKey { get; set; }
+
+        public override object GetValue(
+            IPublishedContent content,
+            Type type,
+            CultureInfo culture,
+            PropertyInfo propertyInfo)
+        {
+            if (string.IsNullOrWhiteSpace(this.DictionaryKey))
+                return null;
+
+            // HACK: [LK:2015-04-14] Resorting to using `UmbracoHelper`, as `CultureDictionaryFactoryResolver` isn't public in v6.2.x.
+            return new UmbracoHelper(UmbracoContext.Current, content).GetDictionaryValue(this.DictionaryKey);
+        }
     }
 }
