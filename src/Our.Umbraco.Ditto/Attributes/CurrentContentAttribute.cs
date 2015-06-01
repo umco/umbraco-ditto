edfe8fc61ae09ea847b7e4ab8e994233fff603e8
leekelleher <leekelleher@gmail.com>
2015-06-01 18:09:26 +0100

    Added attribute and value-resolver for `CurrentContent` (IPublishedContent currently being mapped)

---
+++ src/Our.Umbraco.Ditto/Attributes/CurrentContentAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Our.Umbraco.Ditto
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class CurrentContentAttribute : DittoValueResolverAttribute
+    {
+        public CurrentContentAttribute()
+            : base(typeof(CurrentContentValueResolver))
+        {
+        }
+    }
+}
---
+++ src/Our.Umbraco.Ditto/ComponentModel/ValueResolvers/CurrentContentValueResolver.cs
@@ -0,0 +1,15 @@
+﻿namespace Our.Umbraco.Ditto
+{
+    using System.ComponentModel;
+    using System.Globalization;
+
+    using global::Umbraco.Core.Models;
+
+    public class CurrentContentValueResolver : DittoValueResolver<CurrentContentAttribute>
+    {
+        public override object ResolveValue(ITypeDescriptorContext context, CurrentContentAttribute attribute, CultureInfo culture)
+        {
+            return context.Instance as IPublishedContent;
+        }
+    }
+}
