ad661f21c3e9e19f53a38ad51267cdd84e8e83eb
leekelleher <leekelleher@gmail.com>
2015-06-01 18:55:21 +0100

    Added XML docs

--- src/Our.Umbraco.Ditto/Attributes/CurrentContentAttribute.cs
+++ src/Our.Umbraco.Ditto/Attributes/CurrentContentAttribute.cs
@@ -2,9 +2,16 @@
 {
     using System;
 
+    /// <summary>
+    /// The current content attribute.
+    /// Used for providing Ditto with the current <see cref="IPublishedContent"/> object from Umbraco.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class CurrentContentAttribute : DittoValueResolverAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CurrentContentAttribute"/> class.
+        /// </summary>
         public CurrentContentAttribute()
             : base(typeof(CurrentContentValueResolver))
         {
--- src/Our.Umbraco.Ditto/ComponentModel/ValueResolvers/CurrentContentValueResolver.cs
+++ src/Our.Umbraco.Ditto/ComponentModel/ValueResolvers/CurrentContentValueResolver.cs
@@ -5,8 +5,26 @@
 
     using global::Umbraco.Core.Models;
 
+    /// <summary>
+    /// The current content value resolver.
+    /// </summary>
     public class CurrentContentValueResolver : DittoValueResolver<CurrentContentAttribute>
     {
+        /// <summary>
+        /// Resolves the value.
+        /// Gets the current <see cref="IPublishedContent"/> from Umbraco.
+        /// </summary>
+        /// <param name="context">
+        /// An <see cref="T:System.ComponentModel.ITypeDescriptorContext" /> that provides a format context.
+        /// </param>
+        /// <param name="attribute">
+        /// The <see cref="CurrentContentAttribute"/> containing additional information 
+        /// indicating how to resolve the property.
+        /// </param>
+        /// <param name="culture">The <see cref="T:System.Globalization.CultureInfo" /> to use as the current culture.</param>
+        /// <returns>
+        /// The <see cref="object"/> representing the raw value.
+        /// </returns>
         public override object ResolveValue(ITypeDescriptorContext context, CurrentContentAttribute attribute, CultureInfo culture)
         {
             return context.Instance as IPublishedContent;
