a952b69088a7317cd281243567472acbc4fb6f30
leekelleher <leekelleher@gmail.com>
2015-06-08 08:37:41 +0100

    Formatted the PublishedContent mock builder helpers.

--- tests/Our.Umbraco.Ditto.Tests/Mocks/PropertyBuilder.cs
+++ tests/Our.Umbraco.Ditto.Tests/Mocks/PropertyBuilder.cs
@@ -1,26 +1,25 @@
-﻿using Umbraco.Core.Models;
-
-namespace Our.Umbraco.Ditto.Tests.Mocks
+﻿namespace Our.Umbraco.Ditto.Tests.Mocks
 {
-	public class PropertyBuilder
-	{
-		private string _alias = "alias";
-		private object _value = null;
-		private bool _hasValue = true;
+    using global::Umbraco.Core.Models;
 
-		public static PropertyBuilder Default(string alias, object value)
-		{
-			return new PropertyBuilder
-			{
-				_alias = alias,
-				_value = value
-			};
-		}
+    public class PropertyBuilder
+    {
+        private string _alias = "alias";
+        private object _value = null;
+        private bool _hasValue = true;
 
+        public static PropertyBuilder Default(string alias, object value)
+        {
+            return new PropertyBuilder
+            {
+                _alias = alias,
+                _value = value
+            };
+        }
 
-		public IPublishedContentProperty Build()
-		{
-			return new PublishedContentPropertyMock(_alias, _value, _hasValue);
-		}
-	}
+        public IPublishedContentProperty Build()
+        {
+            return new PublishedContentPropertyMock(_alias, _value, _hasValue);
+        }
+    }
 }
--- tests/Our.Umbraco.Ditto.Tests/Mocks/PublishedContentPropertyMock.cs
+++ tests/Our.Umbraco.Ditto.Tests/Mocks/PublishedContentPropertyMock.cs
@@ -1,23 +1,29 @@
-﻿using System;
-using Umbraco.Core.Models;
-
-namespace Our.Umbraco.Ditto.Tests.Mocks
+﻿namespace Our.Umbraco.Ditto.Tests.Mocks
 {
-	public class PublishedContentPropertyMock : IPublishedContentProperty
-	{
-		public PublishedContentPropertyMock(string @alias, object value, bool hasValue)
-		{
-			HasValue = hasValue;
-			Alias = alias;
-			Value = value;
-		}
+    using System;
+    using global::Umbraco.Core.Models;
 
-		public string PropertyTypeAlias { get; private set; }
-		public bool HasValue { get; private set; }
-		public object DataValue { get; private set; }
-		public object Value { get; private set; }
-		public object XPathValue { get; private set; }
-		public string Alias { get; private set; }
-		public Guid Version { get; private set; }
-	}
+    public class PublishedContentPropertyMock : IPublishedContentProperty
+    {
+        public PublishedContentPropertyMock(string alias, object value, bool hasValue)
+        {
+            HasValue = hasValue;
+            Alias = alias;
+            Value = value;
+        }
+
+        public string PropertyTypeAlias { get; private set; }
+
+        public bool HasValue { get; private set; }
+
+        public object DataValue { get; private set; }
+
+        public object Value { get; private set; }
+
+        public object XPathValue { get; private set; }
+
+        public string Alias { get; private set; }
+
+        public Guid Version { get; private set; }
+    }
 }
