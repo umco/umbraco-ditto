1429aeb8030a7117a5b89e30d67edde8a695f4a5
leekelleher <leekelleher@gmail.com>
2015-06-09 09:52:11 +0100

    Added parameter to enable passing an existing instance of a typed object to `As<T>` .

---
+++ tests/Our.Umbraco.Ditto.Tests/ExistingObjectInstanceTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Our.Umbraco.Ditto.Tests
+{
+    using NUnit.Framework;
+    using Our.Umbraco.Ditto.Tests.Mocks;
+
+    [TestFixture]
+    public class ExistingObjectInstanceTests
+    {
+        public class MyModel
+        {
+            public string MyProperty1 { get; set; }
+
+            [DittoIgnore]
+            public string MyProperty2 { get; set; }
+        }
+
+        [Test]
+        public void Existing_Object_Mapped()
+        {
+            var content = ContentBuilder.Default().Build();
+
+            var value = "Hello world";
+            var model = new MyModel()
+            {
+                MyProperty1 = value,
+                MyProperty2 = value
+            };
+
+            content.As<MyModel>(instance: model);
+
+            Assert.That(model.MyProperty1, Is.Not.EqualTo(value));
+            Assert.That(model.MyProperty2, Is.EqualTo(value));
+        }
+    }
+}
