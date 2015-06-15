0f4488c99a7d16f8efe0bcce6b95e15c121f50b1
leekelleher <leekelleher@gmail.com>
2015-06-15 22:14:25 +0100

    Extended the unit-test to cover using inherited/base classes.

--- tests/Our.Umbraco.Ditto.Tests/ExistingObjectInstanceTests.cs
+++ tests/Our.Umbraco.Ditto.Tests/ExistingObjectInstanceTests.cs
@@ -6,12 +6,18 @@
     [TestFixture]
     public class ExistingObjectInstanceTests
     {
-        public class MyModel
+        public class MyBaseModel
         {
             public string MyProperty1 { get; set; }
 
             [DittoIgnore]
             public string MyProperty2 { get; set; }
+        }
+
+        public class MyInheritedModel : MyBaseModel
+        {
+            [UmbracoProperty("prop3")]
+            public string MyProperty3 { get; set; }
         }
 
         [Test]
@@ -20,16 +26,56 @@
             var content = new PublishedContentMock();
 
             var value = "Hello world";
-            var model = new MyModel()
+            var model = new MyBaseModel()
             {
                 MyProperty1 = value,
                 MyProperty2 = value
             };
 
-            content.As<MyModel>(instance: model);
+            content.As(instance: model);
 
             Assert.That(model.MyProperty1, Is.Not.EqualTo(value));
             Assert.That(model.MyProperty2, Is.EqualTo(value));
+        }
+
+        [Test]
+        public void Existing_Base_Object_Mapped()
+        {
+            var propertyValue = "Foo Bar";
+            var property = new PublishedContentPropertyMock { Alias = "prop3", Value = propertyValue };
+            var content = new PublishedContentMock { Properties = new[] { property } };
+
+            var value = "Hello world";
+            var model = new MyInheritedModel()
+            {
+                MyProperty3 = value
+            };
+
+            // down-cast the model to the base type, so the inherited type properties should not be mapped.
+            content.As<MyBaseModel>(instance: model);
+
+            Assert.That(model.MyProperty3, Is.EqualTo(value));
+            Assert.That(model.MyProperty3, Is.Not.EqualTo(propertyValue));
+        }
+
+        [Test]
+        public void Existing_Inherited_Object_Mapped()
+        {
+            var propertyValue = "Foo Bar";
+            var property = new PublishedContentPropertyMock { Alias = "prop3", Value = propertyValue };
+            var content = new PublishedContentMock { Properties = new[] { property } };
+
+            var value = "Hello world";
+            var model = new MyInheritedModel()
+            {
+                MyProperty3 = value
+            };
+
+            // run through Ditto, the expected behaviour is that the Umbraco property value will overwrite the previous value.
+            content.As(instance: model);
+
+            Assert.That(model.MyProperty3, Is.Not.EqualTo(value));
+            Assert.That(model.MyProperty3, Is.EqualTo(propertyValue));
         }
     }
 }
