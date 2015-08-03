50c493ec874353d38c3127648b264fae67accb61
leekelleher <leekelleher@gmail.com>
2015-08-03 22:36:41 +0100

    Adds BindingFlags to the GetProperty call to make the lookups case insensitive.
    Included unit-test to verify this.
    Fixes #103

---
+++ tests/Our.Umbraco.Ditto.Tests/CaseSensitivityTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Our.Umbraco.Ditto.Tests
+{
+    using NUnit.Framework;
+    using Our.Umbraco.Ditto.Tests.Mocks;
+
+    [TestFixture]
+    public class CaseSensitivityTests
+    {
+        public class MyModel
+        {
+            public string Name { get; set; }
+
+            [UmbracoProperty("Name")]
+            public string Name_PascalCase { get; set; }
+
+            [UmbracoProperty("NAME")]
+            public string Name_UpperCase { get; set; }
+
+            [UmbracoProperty("name")]
+            public string Name_LowerCase { get; set; }
+
+            [UmbracoProperty("NaMe")]
+            public string Name_MixedCase { get; set; }
+        }
+
+        [Test]
+        public void CaseSensitivePropertyName_Mapped()
+        {
+            var name = "MyCustomName";
+
+            var content = new PublishedContentMock
+            {
+                Name = name
+            };
+
+            var model = content.As<MyModel>();
+
+            Assert.That(model.Name, Is.EqualTo(name));
+            Assert.That(model.Name_PascalCase, Is.EqualTo(name));
+            Assert.That(model.Name_UpperCase, Is.EqualTo(name));
+            Assert.That(model.Name_LowerCase, Is.EqualTo(name));
+            Assert.That(model.Name_MixedCase, Is.EqualTo(name));
+        }
+    }
+}
