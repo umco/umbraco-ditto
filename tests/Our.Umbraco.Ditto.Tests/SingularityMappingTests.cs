a22fa598f48421277a5d6e83c9cbe49ca652ce22
leekelleher <leekelleher@gmail.com>
2015-09-21 23:05:45 +0100

    Added a unit-test for mapping a collection of IPublishedContent to a single IPublishedContent property, using the DittoPickerConverter.

---
+++ tests/Our.Umbraco.Ditto.Tests/SingularityMappingTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Our.Umbraco.Ditto.Tests
+{
+    using System.ComponentModel;
+    using System.Linq;
+    using NUnit.Framework;
+    using Our.Umbraco.Ditto.Tests.Mocks;
+    using global::Umbraco.Core.Models;
+
+    [TestFixture]
+    public class SingularityMappingTests
+    {
+        public class MyModel
+        {
+            [TypeConverter(typeof(DittoPickerConverter))]
+            public IPublishedContent MyProperty { get; set; }
+        }
+
+        [Test]
+        public void MyTest()
+        {
+            var mock = new PublishedContentMock();
+            var items = Enumerable.Repeat<IPublishedContent>(mock, 3);
+
+            var property = new PublishedContentPropertyMock()
+            {
+                Alias = "myProperty",
+                Value = items
+            };
+
+            var content = new PublishedContentMock()
+            {
+                Properties = new[] { property }
+            };
+
+            var model = content.As<MyModel>();
+
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model.MyProperty, Is.Not.Null);
+        }
+    }
+}
