3863f53b4f49c229fe3db3817093b38257a86a70
leekelleher <leekelleher@gmail.com>
2015-10-21 15:46:51 +0100

    Added a unit-test to verify the issue raised in #132.
    The problem is that the context in `UmbracoPropertyValueResolver` is not aware of the derived class.
    It only knows about the ComponentType of the class that the property is bound to, (e.g. the inherited/base class).

---
+++ tests/Our.Umbraco.Ditto.Tests/InheritedClassWithPrefixedPropertyTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Our.Umbraco.Ditto.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class InheritedClassWithPrefixedPropertyTests
+    {
+        public class BasicModel
+        {
+            public string MyProperty { get; set; }
+        }
+
+        [UmbracoProperties(Prefix = "inherited_")]
+        public class InheritedModel : BasicModel
+        {
+        }
+
+        [Test]
+        public void InheritedClassWithPrefixedProperty_Mapping()
+        {
+            var value = "foo bar";
+            var property = new Mocks.PublishedContentPropertyMock("inherited_myProperty", value, true);
+            var content = new Mocks.PublishedContentMock() { Properties = new[] { property } };
+
+            var model = content.As<InheritedModel>();
+
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model.MyProperty, Is.EqualTo(value));
+        }
+    }
+}
