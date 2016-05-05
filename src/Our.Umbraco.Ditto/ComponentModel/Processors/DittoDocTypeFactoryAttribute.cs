f8da669557499b94769c91dbb9f6e99b69a80443
leekelleher <leekelleher@gmail.com>
2016-05-05 11:56:01 +0100

    Corrected "sufix" typo; amended code comments

--- src/Our.Umbraco.Ditto/ComponentModel/Processors/DittoDocTypeFactoryAttribute.cs
+++ src/Our.Umbraco.Ditto/ComponentModel/Processors/DittoDocTypeFactoryAttribute.cs
@@ -3,28 +3,28 @@ using Umbraco.Core.Models;
 namespace Our.Umbraco.Ditto
 {
     /// <summary>
-    /// Conveniense processor wrapper for resolving a type based on a content items Doc Type
+    /// Convenient processor wrapper for resolving a type, based on a content item's Doc Type alias.
     /// </summary>
     public class DittoDocTypeFactoryAttribute : DittoFactoryAttribute
     {
         /// <summary>
-        /// A prefix string to prepend onto the resolved type name
+        /// A prefix string to prepend onto the resolved type name.
         /// </summary>
         public string Prefix { get; set; }
 
         /// <summary>
-        /// A sufix string to append onto the resolved type name
+        /// A suffix string to append onto the resolved type name.
         /// </summary>
-        public string Sufix { get; set; }
+        public string Suffix { get; set; }
 
         /// <summary>
-        /// Fetches the type name from the current contents doc type alias
+        /// Fetches the type name from the current content item's Doc Type alias.
         /// </summary>
         /// <param name="currentContent"></param>
         /// <returns></returns>
         public override string ResolveTypeName(IPublishedContent currentContent)
         {
-            return Prefix + currentContent.DocumentTypeAlias + Sufix;
+            return string.Concat(Prefix, currentContent.DocumentTypeAlias, Suffix);
         }
     }
 }
