6f835d8c0a64c4aeba79df50edcfd30d9baf939c
leekelleher <leekelleher@gmail.com>
2016-05-11 19:55:37 +0100

    Added unit-test for setting the default value of ValueType objects

---
+++ tests/Our.Umbraco.Ditto.Tests/ValueTypesTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+using Our.Umbraco.Ditto.Tests.Mocks;
+
+namespace Our.Umbraco.Ditto.Tests
+{
+    [TestFixture]
+    public class ValueTypesTests
+    {
+        public class MyModel
+        {
+            public bool Boolean { get; set; }
+
+            public int Integer { get; set; }
+        }
+
+        [Test]
+        public void ValueTypes_With_Value()
+        {
+            var content = new PublishedContentMock
+            {
+                Properties = new[]
+                {
+                    new PublishedContentPropertyMock("boolean", true, true),
+                    new PublishedContentPropertyMock("integer", 1234, true)
+                }
+            };
+
+            var model = content.As<MyModel>();
+
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model.Boolean, Is.TypeOf<bool>());
+            Assert.That(model.Integer, Is.TypeOf<int>());
+        }
+
+        [Test]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ValueTypes_With_DefaultValue()
+        {
+            var content = new PublishedContentMock();
+
+            var model = content.As<MyModel>();
+
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model.Boolean, Is.TypeOf<bool>());
+            Assert.That(model.Integer, Is.TypeOf<int>());
+        }
+    }
+}
