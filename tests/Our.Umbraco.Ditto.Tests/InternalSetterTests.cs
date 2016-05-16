ed2b86990cbeafc4943968e8b863c472041155db
leekelleher <leekelleher@gmail.com>
2016-05-16 18:56:37 +0100

    Proposed fix for assigning property value of internal setter

---
+++ tests/Our.Umbraco.Ditto.Tests/InternalSetterTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Our.Umbraco.Ditto.Tests.Mocks;
+
+namespace Our.Umbraco.Ditto.Tests
+{
+    [TestFixture]
+    public class InternalSetterTests
+    {
+        public class MyModel
+        {
+            [UmbracoProperty("myProperty")]
+            public string MyProperty1 { get; }
+
+            [UmbracoProperty("myProperty")]
+            public string MyProperty2 { get; set; }
+
+            [UmbracoProperty("myProperty")]
+            public string MyProperty3 { get; internal set; }
+        }
+
+        [Test]
+        public void InternalSetter_Test()
+        {
+            var value = "foo";
+            var content = new PublishedContentMock
+            {
+                Properties = new[] { new PublishedContentPropertyMock("myProperty", value, true) }
+            };
+
+            var model = content.As<MyModel>();
+
+            Assert.That(model.MyProperty1, Is.Null);
+            Assert.That(model.MyProperty2, Is.EqualTo(value));
+            Assert.That(model.MyProperty3, Is.EqualTo(value));
+        }
+    }
+}
