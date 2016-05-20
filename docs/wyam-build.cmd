cf005701ff57927a8c22e7e2d4579fd9b30fb06e
leekelleher <leekelleher@gmail.com>
2016-05-20 18:17:07 +0100

    Added Wyam build to the MSBuild script

---
+++ docs/wyam-build.cmd
@@ -0,0 +1,17 @@
+ECHO OFF
+..\src\.nuget\NuGet.exe install Wyam -Pre -OutputDirectory .wyam
+CD .wyam\Wyam*\tools
+XCOPY /E /H /Y /C *.* ..\..
+CD ..
+RMDIR tools /S /Q
+DEL *.nupkg /Q
+CD ..\..
+ECHO ON
+
+.wyam\Wyam.exe
+
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@EXIT /B 0
+:err
+@PAUSE
+@EXIT /B 1
