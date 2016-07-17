81e60f123df69a6c45bba2036a7ea9418bb26c7a
James Jackson-South <james_south@hotmail.com>
2016-07-17 21:19:59 +1000

    Add multi-single mapping test touch #179

--- tests/Our.Umbraco.Ditto.Tests/SingularityMappingTests.cs
+++ tests/Our.Umbraco.Ditto.Tests/SingularityMappingTests.cs
@@ -5,6 +5,8 @@ using Umbraco.Core.Models;
 
 namespace Our.Umbraco.Ditto.Tests
 {
+    using System.Collections.Generic;
+
     [TestFixture]
     [Category("Mapping")]
     public class SingularityMappingTests
@@ -14,6 +16,14 @@ namespace Our.Umbraco.Ditto.Tests
             public IPublishedContent MyProperty { get; set; }
         }
 
+        public class MyModel2
+        {
+            public IEnumerable<IPublishedContent> MyProperty { get; set; }
+        }
+
+        /// <summary>
+        /// Tests to ensure that collections of items are correctly mapped to a single item.
+        /// </summary>
         [Test]
         public void Single_PublishedContent_Mapped_From_Collection()
         {
@@ -35,6 +45,32 @@ namespace Our.Umbraco.Ditto.Tests
 
             Assert.That(model.MyProperty, Is.Not.Null);
             Assert.That(model.MyProperty, Is.InstanceOf<IPublishedContent>());
+        }
+
+        /// <summary>
+        /// Tests to ensure that single items are correctly mapped to an enumerable.
+        /// </summary>
+        [Test]
+        public void Collection_PublishedContent_Mapped_From_Single()
+        {
+            // make a content node
+            var item = new MockPublishedContent();
+
+            // set the item to a content node's property
+            var content = new MockPublishedContent
+            {
+                Properties = new[] { new MockPublishedContentProperty("myProperty", item) }
+            };
+
+            // map the content node to our model
+            var model = content.As<MyModel2>();
+
+            // check that the model is valid and the property contains a collection (not a single item)
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model, Is.TypeOf<MyModel2>());
+
+            Assert.That(model.MyProperty, Is.Not.Null);
+            Assert.That(model.MyProperty, Is.InstanceOf<IEnumerable<IPublishedContent>>());
         }
     }
 }
