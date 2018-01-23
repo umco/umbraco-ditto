e938fff42c22ae26f90ca027eb70820ee4e49829
leekelleher <leekelleher@gmail.com>
2018-01-23 17:21:00 +0000

    Refactored `DittoDisposableTimer` to be a static class
    This class was creating a LOT of instance allocations.
    By making static, we don't lose the feature, and reduce allocations.

--- src/Our.Umbraco.Ditto/Common/DittoDisposableTimer.cs
+++ src/Our.Umbraco.Ditto/Common/DittoDisposableTimer.cs
@@ -4,25 +4,17 @@ using Umbraco.Core;
 namespace Our.Umbraco.Ditto
 {
     /// <summary>
-    /// Ditto's internal disposable timer for debugging and profiling purposes.
+    /// Ditto's internal static methods for Umbraco's disposable timer (for debugging and profiling purposes).
     /// </summary>
-    internal class DittoDisposableTimer : DisposableTimer
+    internal static class DittoDisposableTimer
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DittoDisposableTimer"/> class.
-        /// </summary>
-        /// <param name="callback">A callback method.</param>
-        public DittoDisposableTimer(Action<long> callback)
-            : base(callback)
-        { }
-
         /// <summary>
         /// Profiles and tracks how long a code fragment takes, until it is disposed.
         /// </summary>
         /// <typeparam name="T">The type of the calling class.</typeparam>
         /// <param name="startMessage">The starting message for the profiler.</param>
         /// <returns>Returns an instance of the disposable timer.</returns>
-        public new static DisposableTimer DebugDuration<T>(string startMessage)
+        public static DisposableTimer DebugDuration<T>(string startMessage)
         {
             return DebugDuration(typeof(T), startMessage);
         }
@@ -35,12 +27,13 @@ namespace Our.Umbraco.Ditto
         /// <returns>Returns an instance of the disposable timer.</returns>
         public static DisposableTimer DebugDuration(Type loggerType, string startMessage)
         {
+#if DEBUG
             if (Ditto.IsProfilingEnabled() && ApplicationContext.Current?.ProfilingLogger != null)
             {
                 return ApplicationContext.Current.ProfilingLogger.DebugDuration(loggerType, startMessage, "Complete");
             }
-
-            return new DittoDisposableTimer((x) => { });
+#endif
+            return default(DisposableTimer);
         }
     }
 }
